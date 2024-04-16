cb5bb1c3f8b18782a7ecc60e12b4d3a29d78c706
Giorgos Skourtsidis <gskourts@gmail.com>
2024-04-17 02:22:30 +0300

    initial commit

---
+++ src/main/java/org/jobrunr/test/carbonaware/CarbonAwareApplication.java
@@ -0,0 +1,13 @@
+package org.jobrunr.test.carbonaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonAwareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/jobrunr/test/carbonaware/TaskController.java
@@ -0,0 +1,21 @@
+package org.jobrunr.test.carbonaware;
+
+import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.utils.carbonaware.CarbonAwarePeriod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+import static java.time.temporal.ChronoUnit.HOURS;
+
+@RestController
+public class TaskController {
+
+    @GetMapping("/schedule-carbon-aware-task")
+    public void getAllTasks(@RequestParam String message, @RequestParam int hoursAheadDeadline) {
+        BackgroundJob.scheduleCarbonAware(CarbonAwarePeriod.before(Instant.now().plus(hoursAheadDeadline, HOURS)),
+                () -> System.out.println("Carbon Aware Task: " + message));
+    }
+}
