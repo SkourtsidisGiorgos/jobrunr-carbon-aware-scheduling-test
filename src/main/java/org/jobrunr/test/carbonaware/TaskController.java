ffa03737cc69b4fd2d898723891a54b26f7871af
Giorgos Skourtsidis <gskourts@gmail.com>
2024-04-23 17:19:49 +0300

    remove not-needed schedule-carbon-aware-task

--- src/main/java/org/jobrunr/test/carbonaware/TaskController.java
+++ src/main/java/org/jobrunr/test/carbonaware/TaskController.java
@@ -14,12 +14,6 @@ import static java.time.temporal.ChronoUnit.HOURS;
 public class TaskController {
 
     @GetMapping("/schedule-carbon-aware-task")
-    public void createNewTask(@RequestParam String message, @RequestParam int hoursAheadDeadline) {
-        BackgroundJob.scheduleCarbonAware(CarbonAwarePeriod.before(Instant.now().plus(hoursAheadDeadline, HOURS)),
-                () -> System.out.println("Carbon Aware Task: " + message));
-    }
-
-    @GetMapping("/schedule-carbon-aware-task-between")
     public void createNewTaskBetween(@RequestParam String message, @RequestParam int startHoursAhead, @RequestParam int endHoursAhead) {
         BackgroundJob.scheduleCarbonAware(CarbonAwarePeriod.between(Instant.now().plus(startHoursAhead, HOURS), Instant.now().plus(endHoursAhead, HOURS)),
                 () -> System.out.println("Carbon Aware Task: " + message));
